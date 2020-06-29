68c5a0103c2ad5d1b3a52192f7c8f3b9f4bc3b0b
fox.wang <fox3000wang@hotmail.com>
2020-06-29 13:42:34 +0800

    流程基本跑通

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import dva from 'dva';
 import './index.css';
+import {
+  createBrowserHistory as createHistory
+} from 'history';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  history: createHistory(),
+  initialState: {
+    products: [{
+        name: 'dva',
+        id: 1
+      },
+      {
+        name: 'antd',
+        id: 2
+      },
+    ],
+  },
+});
 
 // 2. Plugins
 // app.use({});
 
 // 3. Model
-// app.model(require('./models/example').default);
+app.model(require('./models/products').default);
 
 // 4. Router
 app.router(require('./router').default);
